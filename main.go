77dfeeec7646449b89ca794ab7b57a905671cb83
fisayo-dev <olufisayobadina@gmail.com>
2025-12-13 17:34:12 +0100

    deps: install godotenv

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT is not found in the environment")
