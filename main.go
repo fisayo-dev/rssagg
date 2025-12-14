05574dcec10fd96d67e4d3a83a5063e3dfe163aa
fisayo-dev <olufisayobadina@gmail.com>
2025-12-14 15:41:04 +0100

    fix: scope handler to only refire on GET request

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 	// }))
 
 	v1Router := chi.NewRouter()
-	v1Router.HandleFunc("/healthz", handlerHealthz)
+	v1Router.Get("/healthz", handlerHealthz)
 
 	// Mount base router to v1 router
 	router.Mount("/v1", v1Router)
